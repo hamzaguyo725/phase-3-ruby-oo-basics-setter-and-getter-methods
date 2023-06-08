6279141a3c069666661013012ec33c8058497339
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-06-08 23:55:34 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
 
+      attr_accessor :name, :breed
+
+    def name=(name)
+        @name=name
+    end
+
+    def breed=(value)
+        @breed=value
+    end
+
+    def name
+        @name
+        @breed
+    end
+
 end
+# fido = Dog.new
+# puts fido.name = "Fido"
+# snoopy = Dog.new
+# puts snoopy.breed = "Beagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+     
+    attr_accessor :name, :job
+
+    def name=(name)
+        @name=name
+    end
+
+    def job=(value)
+        @job=value
+    end
+
+    def name
+        @name
+        @job
+    end
 
 end
+
+# beyonce = Person.new
+# puts beyonce.name="Beyonce"
+# puts beyonce.job="Singer"
